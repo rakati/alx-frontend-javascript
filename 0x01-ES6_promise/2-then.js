7259b838f9144a58d571507d51a7adb9750222e0
rakati <ouhaddounouredine@gmail.com>
2024-11-20 19:01:59 +0100

    Fix: add return keyword and throw empty error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
     .then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Error())
+    .catch(() => {
+      throw new Error();
+    })
     .finally(() => {
       console.log('Got a response from the API');
     });
